321f81ffe9b8d975f239ca4625544d4285491d5f
subrata34 <subratamodak3491@gmail.com>
2021-08-08 12:46:36 +0600

    subrata

---
+++ array.js
@@ -0,0 +1,34 @@
+//uniqe=[];
+//     for (let i=0;i<a.length;i++){
+//       const element=a[i];
+//     }
+//     for(const element of a){
+//         console.log(element);
+//         if(uniqe.indexOf(element)==-1){
+//               uniqe.push(element);
+//         }
+//     }
+//     return uniqe;
+// }
+
+// const uniqeueName =removeable(a);
+// console.log(uniqeueName);
+// var  mystring ='Hallo How are you ';
+// console.log(mystring);
+// for(const element of mystring){
+//     console.log(element);
+// }
+// function array(num1,num2){
+//     var sum=num1+num2;
+//     return sum ;
+// }
+
+var factorial =array(25,40);
+console.log(factorial );
+function multiply(N1,N2){
+    var multiply=N1/N2;
+    return multiply;
+}
+var m=multiply(25,5)
+console.log(m);
+
