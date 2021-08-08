39439323b3ea80e5d4dc1bb21df260bd7f06bab5
subrata34 <subratamodak3491@gmail.com>
2021-08-09 03:54:42 +0600

    commit

---
+++ assgnment.js
@@ -0,0 +1,7 @@
+
+// function SeerToMon(number){
+//     var seer =(1/40)*number;
+//     return (seer+1);
+// }
+// var IntigerNumber=SeerToMon('input integer value of  seer :',45);
+// console.log('output value of mon  :',IntigerNumber);
---
+++ total-price.js
@@ -0,0 +1,28 @@
+const products=[
+    {
+        name :'laptop',
+        price :20000
+    },
+    {
+        name :'phone',
+        price :30000
+    },
+    {
+        name :'watch',
+        price :240000
+    },
+    {
+        name :'white board',
+        price :80000
+    },
+    {
+        name :'keyboard',
+        price :50000
+    },
+    {
+        name :'harican',
+        price :60000,
+    }
+
+];
+console.log(products);
