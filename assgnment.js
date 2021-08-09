125eb74d77a0746ce79b3446e051566b42bb97a6
subrata34 <subratamodak3491@gmail.com>
2021-08-09 11:46:00 +0600

    sum

--- assgnment.js
+++ assgnment.js
@@ -1,23 +1,29 @@
 
-// function SeerToMon(number){
-//     var seer =(1/40)*number;
-//     return (seer+1);
-// }
-// var IntigerNumber=SeerToMon('input integer value of  seer :',45);
-// console.log('output value of mon  :',IntigerNumber);
 
-//Input three variable get total output variable 
-// function totalSale(price1,price2,price3){
-//     var shirt=200*price1;
-//     var shoes=300*price2;
-//     var hudi=400*price3;
-//     var Totalprice =shirt +shoes+hudi;
-//    return Totalprice ;
-// }
+/*
+function SeerToMon(number){
+    var seer =(1/40)*number;
+    return (seer+1);
+}
+var IntigerNumber=SeerToMon('input integer value of  seer :',45);
+console.log('output value of mon  :',IntigerNumber);
+*/
+
+/*
+Input three variable get total output variable 
+function totalSale(price1,price2,price3){
+    var shirt=200*price1;
+    var shoes=300*price2;
+    var hudi=400*price3;
+    var Totalprice =shirt +shoes+hudi;
+   return Totalprice ;
+}
 
 
-// var quantity =totalSale(1,2,1);
-// console.log(quantity);
+var quantity =totalSale(1,2,1);
+console.log(quantity);
+*/
+
 /*
 function deliveryCost(quantity){ 
     var firstprice =0;
